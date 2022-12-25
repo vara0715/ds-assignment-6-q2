2dbef75b325107fc97fb90499a98601d7fec8c21
vara0715 <88980746+vara0715@users.noreply.github.com>
2022-12-25 20:22:58 +0530

    Add files via upload

---
+++ ds-assignment-6-1-1.py
@@ -0,0 +1,7 @@
+import json
+	
+# Data to be written
+State_Capital ={
+"kerala": "Trivendram","karnataka": "Bengaluru","Tamilnadu": "Chennai","Madhya Pradesh": "Bhopal","West Bengal" : "Kolkata","Telangana" : "Hyderabad","Gujarat" : "Gandhinagar"
+}
+print(state_capital)
